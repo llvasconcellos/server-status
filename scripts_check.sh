2c886403aaff441e5684f35b46e348f3143b9bd3
robinedwards <geotheory1@gmail.com>
2016-02-01 00:29:32 +0000

    updated scripts_check for LCD

--- scripts_check.sh
+++ scripts_check.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Runs in background to assess if server/battery status reporting scripts are working properly
-# Creates/kills instances of report_failed_process.py to report failures
 
 cd /home/root/gpio
 
@@ -17,25 +16,9 @@ while true; do
 	batt=$(echo "$batt_lines" | sed '/^\s*$/d' | wc -l);  # sed to ignore empty lines
 	serv=$(echo "$serv_lines" | sed '/^\s*$/d' | wc -l);
 
-	# kill any excess script instances
-	# [ $batt -gt 1 ] && for i in $(seq $(expr $batt - 1)); do kill $(echo "$batt_lines" | sed -n $i'p' | grep -oE '[0-9]+' | head -n 1); echo duplicate process killed >> sd/battery_status_log.txt; done
-	# [ $serv -gt 1 ] && for i in $(seq $(expr $serv - 1)); do kill $(echo "$serv_lines" | sed -n $i'p' | grep -oE '[0-9]+' | head -n 1); echo duplicate process killed >> sd/server_status_log.txt; done 
-
-	# count active failed-script reporting scripts
-	failbatt=$(echo "$prs" | grep "report_failed_process.py battery" | grep -v grep | sed '/^\s*$/d' | wc -l);
-	failserv=$(echo "$prs" | grep "report_failed_process.py server" | grep -v grep | sed '/^\s*$/d' | wc -l);
-
 	# Report failures to LCD if new
-	[ $batt == 0 ] && [ $failbatt == 0 ] && echo 'battery failed' && python report_failed_process.py battery &
-	[ $serv == 0 ] && [ $failserv == 0 ] && echo 'server failed' && python report_failed_process.py server & 
+	[ $batt == 0 ] && echo 'battery_status.py failed'
+	[ $serv == 0 ] && echo 'server_status.py failed' && python /home/root/gpio/lcd/report_lines_and_battery.py 'Server info not' 'reporting.' &
 
-	# Kill any script-failed processes that are no longer correct
-	[ $failbatt == 1 ] && [ $batt -gt 0 ] echo 'kill ' && && kill $(echo "$prs" | grep 'report_failed_process.py battery' | grep -v grep | grep -oE '[0-9]+' | head -n 1)
-	[ $failserv == 1 ] && [ $serv -gt 0 ] && kill $(echo "$prs" | grep 'report_failed_process.py server' | grep -v grep | grep -oE '[0-9]+' | head -n 1)
-
-	# Log new script failures
-	[ $batt != 1 ] && [ $failbatt == 0 ] && printf "battery_status.py "$batt" instance(s)\\n" >> sd/battery_status_log.txt
-	[ $serv != 1 ] && [ $failserv == 0 ] && printf "server_status.py "$serv" instance(s)\\n" >> sd/server_status_log.txt
-	
-	sleep 15
+	sleep 60
 done
